c0c37c34abbca60eee678f7332fcb9a862d6d2d3
sarithakurella <sarukurella@gmail.com>
2024-06-11 23:29:50 +0530

    Adding data types

---
+++ Excercise_10-06-2024/Task2.py
@@ -0,0 +1,8 @@
+# Python script that calculates the square and cube of a number
+
+num = input("Enter a number: ")
+a = int(num) ** 2
+print(f"square of a number:{a}")
+
+b = int(num) ** 3
+print(f"cube of a number:{b}")
