99bcf287f887e89f75ab78d573b93c7a94360101
sarithakurella <sarukurella@gmail.com>
2024-06-11 23:44:46 +0530

    task

--- Excercise_10-06-2024/Task2.py
+++ Excercise_10-06-2024/Task2.py
@@ -6,3 +6,8 @@ print(f"square of a number:{a}")
 
 b = int(num) ** 3
 print(f"cube of a number:{b}")
+
+#python program to take the input for two number and print if num1 is greater than num2 or less than or equal to num2
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("number1 is greater than number 2" if num1>num2 else "number1 is less than or equal to number 2" if num1<num2 else "number 1 is equal to number 2" if num1 == num2 else "invalid input")
